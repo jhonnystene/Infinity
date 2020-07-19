5863fb1fa2a3aa1ae9a3ad4df0882534c1eb73d9
Johnny Stene <jhonnystene@gmail.com>
2020-07-19 15:44:55 -0700

    Add license text to top of files

--- net/ddns/johnnystene/infinitytoolkit/engine/Keyboard.java
+++ net/ddns/johnnystene/infinitytoolkit/engine/Keyboard.java
@@ -1,3 +1,24 @@
+/*
+ * Infinity Game Engine
+ * Copyright (C) 2020 Johnny Stene
+
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+
+ */
+ 
 // No, I didn't write this from scratch. Wrote a python script to do it for me haha
 
 package net.ddns.johnnystene.infinitytoolkit.engine;
